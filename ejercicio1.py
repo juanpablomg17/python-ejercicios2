347819e018442bdd6794a27265406d243e7d2277
juanpablomg17 <58058546+juanpablomg17@users.noreply.github.com>
2020-02-27 19:09:15 -0500

    listo

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,8 +1,12 @@
 
 
 def numeropares(n1,n2):
-  
-   for i  in range(n1,n2+1):
+  if n1<n2:
+      for i  in range(n1,n2+1):
+           if ((i%2) == 0):
+               print(i)
+  elif n1>n2:
+       for i  in range(n2,n1+1):
            if ((i%2) == 0):
                print(i)
 pass
@@ -14,10 +18,17 @@ def pedirNumeroEntero():
     
     while(not correcto):
         try:
-            n1 = int(input("Introduce un numero entero: "))
-            n2 = int(input("Introduce un número entero: "))
+
+            n1 = int(input("Introduzca el número menor del rango: "))
+            n2 = int(input("Introduzca el número mayor del rango: "))
             numeropares(n1,n2)
             correcto=True
+             
+      
+
+
+         
+            
 
             
         except ValueError:
